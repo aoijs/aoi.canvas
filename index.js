ac3a6234785ddfe3a7030ce06ba315138705934a
Lordex <130374125+LordexDuck3990@users.noreply.github.com>
2023-07-15 16:19:03 +0300

    +ver

--- index.js
+++ index.js
@@ -28,13 +28,11 @@ module.exports = {
             console.log(`\x1b[96m|---------------|\n|---\x1b[97mAoiCanvas\x1b[96m---|\n|----\x1b[97mLoaded.\x1b[96m----|\n|-----\x1b[97maoiv6\x1b[96m-----|\n|---------------|\x1b[0m`);
         } else {
             canvaError.newWarn("Version '"+vers+"' not found!!!!! please set version to \"v6\". If you dont change it to supported version aoi.canvas will not work!")
-        }
+        };
 
         if (et !== "console" && et !== "msg" && et !== "message" && et !== "none") {
             canvaWarn.newWarn(`Unknown 'ErrorsType' option type. AoiCanvas Errors would not be shown.`);
-        }
-
-        console.log(`\x1b[96m|---------------|\n|---\x1b[97mAoiCanvas\x1b[96m---|\n|----\x1b[97mLoaded.\x1b[96m----|\n|---------------|\x1b[0m`);
+        };
     },
 
     getData: () => {

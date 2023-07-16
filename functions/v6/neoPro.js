7cac75ae76fc46e4b26c3472b49004bcd9277992
Lordex <130374125+LordexDuck3990@users.noreply.github.com>
2023-07-16 21:53:00 +0300

    1.0.3

--- functions/v6/neoPro.js
+++ functions/v6/neoPro.js
@@ -1,7 +1,7 @@
 let { loadImage, createCanvas } = require("@napi-rs/canvas");
 
 module.exports = {
-    name: "$lerefPro",
+    name: "$neoPro",
     type: "djs",
     code: async (d) => {
         const data = d.util.aoiFunc(d);
@@ -9,25 +9,25 @@ module.exports = {
             d.data.canvases = {};
         };
 
-        if (!d.data.canvases["lerefPro"]) {
+        if (!d.data.canvases["neoPro"]) {
             if (!d.data.canvases) {
                 d.data.canvases = {};
             };
-            var Leref = await d.util.getUser(d, '608358453580136499')
-            Leref = Leref.avatarURL({format: 'png', size: 4024, dynamic: true});
-            
+            var Neo = await d.util.getUser(d, '285118390031351809')
+            Neo = Neo.avatarURL({format: 'png', size: 4096, dynamic: true});
+
             var canv = createCanvas(4024, 4024);
             var canvas = {
                 "canvas": canv,
                 "ctx": canv.getContext("2d")
             };
 
-            await loadImage(Leref).then((image) => {
+            await loadImage(Neo).then((image) => {
                 canvas.ctx.drawImage(image, 1, 1, 4024, 4024);
                 canvas.ctx.font = "450px Impact";
                 canvas.ctx.fillText("Pro", 1850, 3000)
 
-                d.data.canvases["lerefPro"] = canvas;
+                d.data.canvases["neoPro"] = canvas;
             });
         };
 
